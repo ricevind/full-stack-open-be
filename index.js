a7e09b18e908c5a95dca0368e471ac4ee106c183
Michal <michal.kwasniewski@dotdata.com>
2021-11-12 18:18:26 +0100

    import fixup

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors')
-const static = require('')
 const initPersons = require('./persons.json')
 
 let persons = [...initPersons];
