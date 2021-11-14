50e935088d35da6f6419837310fcc07d3e896efe
Michal <michal.kwasniewski@dotdata.com>
2021-11-14 18:49:31 +0100

    part3:3.22

--- backend/index.js
+++ backend/index.js
@@ -4,33 +4,33 @@ const mongoose = require('mongoose');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const { personModel } = require('./models')
+const { personModel } = require('./models');
 
 mongoose.connect(process.env.DATABASE_URI);
 const app = express();
 
 
-app.use(cors())
-app.use(express.static('static'))
-app.use(express.json())
+app.use(cors());
+app.use(express.static('static'));
+app.use(express.json());
 
-morgan.token('body', (req) => console.log(req.body) || JSON.stringify(req.body))
-app.use(morgan(':method :url :status :total-time[3] :body'))
+morgan.token('body', (req) => console.log(req.body) || JSON.stringify(req.body));
+app.use(morgan(':method :url :status :total-time[3] :body'));
 
 
 
 app.get('/api/persons', (_, res) => {
-    personModel.getPersons().then(persons => res.json(persons))
+    personModel.getPersons().then(persons => res.json(persons));
 });
 
 app.get('/api/info', (req, res) => {
     personModel.getPersons().then(persons => {
         const message = `Phonebook has info for ${persons.length} people`;
         const date = (new Date()).toLocaleString('pl-PL', { dateStyle: 'full', timeStyle: 'long' });
-        const info = `${message}\n\r${date}`
+        const info = `${message}\n\r${date}`;
 
-        res.send(info)
-    })
+        res.send(info);
+    });
 });
 
 app.get('/api/persons/:id', (req, res, next) => {
@@ -46,8 +46,8 @@ app.get('/api/persons/:id', (req, res, next) => {
         res.statusMessage = 'Person not found';
         res.status(404);
         res.send();
-    }).catch(e => next(e))
-})
+    }).catch(e => next(e));
+});
 
 
 app.delete('/api/persons/:id', (req, res, next) => {
@@ -57,7 +57,7 @@ app.delete('/api/persons/:id', (req, res, next) => {
         res.status(204);
         res.send();
     }).catch(e => next(e));
-})
+});
 
 app.put('/api/persons/:id', (req, res, next) => {
     const person = req.body;
@@ -72,8 +72,8 @@ app.put('/api/persons/:id', (req, res, next) => {
         res.statusMessage = 'Person not found';
         res.status(404);
         res.send();
-    }).catch(e => next(e))
-})
+    }).catch(e => next(e));
+});
 
 app.post('/api/persons', (req, res, next) => {
 
@@ -81,33 +81,33 @@ app.post('/api/persons', (req, res, next) => {
 
     personModel.addPerson(personCandidate).then(person => {
         res.status(201);
-        res.json(person)
-    }).catch(e => next(e))
+        res.json(person);
+    }).catch(e => next(e));
 
-})
+});
 
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-}
-app.use(unknownEndpoint)
+    response.status(404).send({ error: 'unknown endpoint' });
+};
+app.use(unknownEndpoint);
 
 
 function handleError(error, req, res, next) {
     if (error.name === 'CastError') {
-        return res.status(400).send({ error: 'malformatted id' })
+        return res.status(400).send({ error: 'malformatted id' });
     }
 
     if (error.name === 'ValidationError') {
-        console.error(JSON.stringify(error, null, 2))
-        return res.status(400).send({ error: error.message })
+        console.error(JSON.stringify(error, null, 2));
+        return res.status(400).send({ error: error.message });
     }
 
-    next(error)
+    next(error);
 }
-app.use(handleError)
+app.use(handleError);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 
-console.log(`Server is listening on ${PORT}`)
+console.log(`Server is listening on ${PORT}`);
--- backend/models/person.js
+++ backend/models/person.js
@@ -8,7 +8,7 @@ const personSchema = new mongoose.Schema({
     number: {
         type: String, required: true
     },
-})
+});
 
 personSchema.set('toJSON', {
     transform: (_, returnedObject) => {
@@ -17,38 +17,38 @@ personSchema.set('toJSON', {
         delete returnedObject.__v;
     }
 });
-personSchema.plugin(uniqueValidator)
+personSchema.plugin(uniqueValidator);
 
 const Person = new mongoose.model('Person', personSchema);
 
 
 function addPerson({ name, number }) {
     const person = new Person({ name, number });
-    return person.save()
+    return person.save();
 }
 
 function getPersons() {
-    return Person.find({})
+    return Person.find({});
 }
 
 function getPerson(id) {
-    return Person.findById(id)
+    return Person.findById(id);
 }
 
 function deletePerson(id) {
-    return Person.findByIdAndRemove(id)
+    return Person.findByIdAndRemove(id);
 }
 
 function isNameTaken(name) {
     return Person.find({ name }).then(p => {
         return p.length > 0;
-    })
+    });
 }
 
 function updatePerson(personId, person) {
-    return Person.findByIdAndUpdate(personId, person, { new: true })
+    return Person.findByIdAndUpdate(personId, person, { new: true });
 }
 
 module.exports.person = {
     addPerson, getPersons, getPerson, deletePerson, isNameTaken, updatePerson
-}
+};
